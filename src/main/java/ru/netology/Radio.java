1bbdabd73effc01aa83d8e58cedce3981315bb37
ValiullinAd <93069873+ValiullinAd@users.noreply.github.com>
2022-01-03 14:15:15 +0300

    Update Radio.java

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -5,8 +5,10 @@ public class Radio {
     private int currentRadioStation;
 
     public int getCurrentRadioStation() {
+
         return currentRadioStation;
     }
+
 
     public void setCurrentRadioStation(int currentRadioStation) {
 
@@ -22,18 +24,14 @@ public class Radio {
     }
 
     public  void setUpRadioStation(int currentRadioStation) {
-        //if (currentRadioStation >= 0 || currentRadioStation < 9) {
             this.currentRadioStation = currentRadioStation + 1;
-        //}
-        if (currentRadioStation >= 9) {
+        if (this.currentRadioStation >9) {
             this.currentRadioStation = 0;
         }
     }
     public  void setDownRadioStation(int currentRadioStation) {
-        if (currentRadioStation >= 0 || currentRadioStation < 9) {
             this.currentRadioStation = currentRadioStation - 1;
-        }
-        if (currentRadioStation <= 0) {
+        if (this.currentRadioStation < 0) {
             this.currentRadioStation = 9;
         }
     }
@@ -57,20 +55,15 @@ public class Radio {
         this.currentVolume = currentVolume;
     }
     public  void setDownVolume(int currentVolume) {
-        if (currentVolume > 10 || currentVolume >= 0) {
 
-            this.currentVolume = currentVolume - 1;
-
-        }
-        if (currentVolume <= 0) {
+        if (currentVolume <= -1) {
             this.currentVolume = 0;
         }
     }
     public  void setUpVolume(int currentVolume) {
-        if (currentVolume >= 0 || currentVolume >=9) {
+
             this.currentVolume = currentVolume + 1;
-       }
-        if (currentVolume >= 9) {
+        if (this.currentVolume > 9) {
             this.currentVolume = 10;
         }
     }
